b094f0b3edcb80beef29429acd5bfaa6b36fcb9a
yes-xodnd <taew3167@gmail.com>
2021-04-08 05:17:12 +0900

    Add debug log for web worker

--- worker.js
+++ worker.js
@@ -2,7 +2,8 @@ import fns from './lib/iteration.js';
 import test from './lib/test.js';
 
 onmessage = ({ data }) => {
-  const { L, N } = data; 
+  const { L, N } = data;
+  console.log('worker engaged: ', fns, L, N);
   const result = test({ fns, L, N });
   postMessage(result);
 };
