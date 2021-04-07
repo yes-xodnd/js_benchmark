2275e8cc4ed8e7ca4bda31b4f6278c5f2576c62c
yes-xodnd <taew3167@gmail.com>
2021-04-08 05:02:21 +0900

    Delete console.log

--- worker.js
+++ worker.js
@@ -1,11 +1,8 @@
 import fns from './lib/iteration.js';
 import test from './lib/test.js';
 
-
-
 onmessage = ({ data }) => {
   const { L, N } = data; 
-  console.log('worker activated');
   const result = test({ fns, L, N });
   postMessage(result);
 };
