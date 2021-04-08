920ddb932cfa0fc04f4d6203f107ba76b897d171
yes-xodnd <taew3167@gmail.com>
2021-04-08 22:06:41 +0900

    Add onresize on window

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import About from './components/About.js';
 import { setCssVarHeaderHeight } from './lib/setCssVar.js';
 
 setCssVarHeaderHeight();
+window.addEventListener('resize', setCssVarHeaderHeight);
+
 App();
 About();
 
